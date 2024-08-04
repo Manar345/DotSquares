4968c996f9b83987aa80f40da87a9eb181a37128
Manar <manarmustafa210@gmail.com>
2024-08-05 01:09:21 +0300

    add the second slider and CTA section

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ var slider1 = new Swiper(".auto-swiper", {
     },
   },
 });
+var slider2 = new Swiper(".auto-swiper2", {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    pauseOnMouseEnter: "false",
+  },
+  spaceBetween: 20,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    },
+  },
+  pagination: {
+    el: ".advanced-swiper-pagination",
+  },
+});
